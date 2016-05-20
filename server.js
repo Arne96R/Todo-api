910829e14fcb320c6bb4bf2b1c692ce25fa52496
Arne de Roode <arneder@gmail.com>
2016-05-20 12:59:23 +0200

    Refactored API with underscore

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -20,15 +21,8 @@ app.get('/todos', function (req, res) {
 //GET /todos/:id
 app.get('/todos/:id', function (req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo;
-
-	todos.forEach(function (todo) {
-		if (todoId === todo.id) {
-			matchedTodo = todo;
-		}
-	})
-	// Iterate over todos array. find the match
-	
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+		
 	if (matchedTodo) {
 		res.json(matchedTodo);
 	}	else {
@@ -38,12 +32,18 @@ app.get('/todos/:id', function (req, res) {
 
 //POST /todos
 app.post('/todos', function (req, res) {
-	var body = req.body;
+	var body = _.pick(req.body, 'description', 'completed');
+
+	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+		return res.status(400).send();
+	}
+
+	body.description = body.description.trim();
 
 	body.id = todoNextId++;
 
 	todos.push(body);
-	//add id field
+	//add id field 
 	//push body into array
 
 	res.json(body);
