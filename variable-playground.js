9427935f7740aab92ee1fb06d95d4dc26d045446
Arne de Roode <arneder@gmail.com>
2016-05-22 00:53:19 +0200

    add variable playground area

---
+++ variable-playground.js
@@ -0,0 +1,30 @@
+// var person = {
+// 	name: 'Arne',
+// 	age: 21
+// };
+
+// function updatePerson(obj) {
+// 	// obj = {
+// 	// 	name: 'Arne',
+// 	// 	age: 19
+// 	// };
+// 	obj.age = 24;
+// }
+
+
+// updatePerson(person);
+// console.log(person);
+
+//array example
+var grades = [7, 10];
+
+function addGrade(gradeList) {
+	gradeList.push(8);
+	debugger;
+
+	//gradeList = [7, 10, 8];
+}
+
+addGrade(grades);
+console.log(grades);
+
