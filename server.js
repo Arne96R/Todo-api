97e4efee2da0930ff99ddea2c4ec8fc3fbc8e462
Arne de Roode <arneder@gmail.com>
2016-06-25 22:25:22 +0200

    added DELET users/login (logout)

--- server.js
+++ server.js
@@ -197,17 +197,26 @@ app.post('/users', function (req, res) {
 
 app.post('/users/login', function (req, res) {
 	var body = _.pick(req.body, 'email', 'password');
+	var userInstance;
 
 	db.user.authenticate(body).then(function (user) {
 		var token = user.generateToken('authentication');
+		userInstance=user;
 
-		if (token) {
-			res.header('Auth', token).json(user.toPublicJSON());	
-		} else {
-			res.status(401).send();
-		}
+		return db.token.create({
+			token:token
+		});
+
+		// if (token) {
+		// 	res.header('Auth', token).json(user.toPublicJSON());	
+		// } else {
+		// 	res.status(401).send();
+		// }
 		
-	}, function () {
+	}).then(function (tokenInstance) {
+		console.log(tokenInstance.get('token'));
+		res.header('Auth', tokenInstance.get('token')).json(userInstance.toPublicJSON());
+	}).catch(function () {
 		res.status(401).send();
 	});
 
@@ -229,6 +238,16 @@ app.post('/users/login', function (req, res) {
 	// 	res.status(500).senc();
 	// });
 });
+
+// DELETE /users/login
+app.delete('/users/login', middleware.requireAuthentication, function (req, res) {
+	req.token.destroy().then(function () {
+		res.status(204).send();
+	}).catch(function () {
+		res.status(500).send();
+	})
+})
+
 
 db.sequelize.sync({force: true}).then(function() {
 	app.listen(PORT, function() {
